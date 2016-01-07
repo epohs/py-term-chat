81da6bc4452f84cfa68ac98bcc760ff707d09cc7
Jason Shope <shope@cakewebsites.com>
2016-01-07 17:42:08 -0500

    fiddling with curses

--- screen_layout.py
+++ screen_layout.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import curses
-import curses.textpad
 import time
 from pprint import pprint
 
@@ -10,32 +9,45 @@ screen = curses.initscr()
 try:
 	(h, w) = screen.getmaxyx()
 
-	curses.noecho()
-	#curses.echo()
 
+	#curses.noecho()
+	curses.echo()
+	curses.cbreak()
+	curses.start_color()
+	curses.use_default_colors()
 
-	chat = curses.newwin(h, w, 0, 0)
+	chat = curses.newpad(10000, w)
 	input_window = curses.newwin(3, w, int(h-3), 0)
 	
-	input_window.box()
-	#tb = curses.textpad.Textbox(input_window)
-	#text = tb.edit()
-	#curses.addstr(4,1,dims.encode('utf_8'))
+	#input_window.box()
 
-	#hw = "Hello world!"
+
+
 
 	in_str = ''
+	ins_row = 0
+	scroll_top = 0
+
 
 	while 1:
 		c = input_window.getch()
 		if c == curses.KEY_ENTER or c == 10 or c == 13:
-			chat.addstr(0, 0, in_str.encode('utf_8'))
-			chat.refresh()
-			screen.erase()
+
+			if ins_row >= int(h-3):
+				scroll_top += 1
+
+			chat.addstr(ins_row, 0, 'me: ')
+			chat.addstr(ins_row, 4, in_str.encode('utf_8'), curses.A_DIM)
+			chat.addstr(ins_row+1, 0, 'them: ', curses.A_BOLD)
+
+			chat.refresh(scroll_top, 0, 0, 0, int(h-3), w)
+			input_window.erase()
+			in_str = ''
+			ins_row += 1
 		else:
+			# Add this character to the string that 
+			# we will insert later
 			in_str += chr(c).encode('utf_8')
-			chat.insch(0, (w-1), c)
-			chat.refresh()
 
 
 except KeyboardInterrupt:
